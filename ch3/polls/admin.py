4f87098749f0548bbd567ef02ecdd4cd46aa56bc
makemeha2 <makemeha2@gmail.com>
2021-03-29 15:55:00 +0900

    4장 마무리

--- ch3/polls/admin.py
+++ ch3/polls/admin.py
@@ -1,5 +1,19 @@
 from polls.models import Question, Choice
 from django.contrib import admin
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question Statement', {'fields':['question_text']}),
+        ('Date Information', {'fields':['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text','pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
